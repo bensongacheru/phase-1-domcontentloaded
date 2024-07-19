d187c8350b1e357afcbe49fb4e254c2b91b4a6bd
benson <mwangibenson923@gmail.com>
2024-07-19 17:10:52 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  
+    // Your code to manipulate the DOM goes here
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+  });
+  // Your code goes here
